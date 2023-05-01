75c8483845b7b2a185aa0ebb03348629bcb87968
andreasvirkus <andreasvirkus@gmail.com>
2023-05-02 01:32:17 +0300

    Add getServerSession example

--- server/api/healthz.ts
+++ server/api/healthz.ts
@@ -1,5 +1,10 @@
-export default defineEventHandler((event) => {
-    return {
-        service: 'online'
-    }
+import { getServerSession } from '#auth'
+
+export default defineEventHandler(async (event) => {
+  const session = await getServerSession(event)
+  console.info('sesh', session)
+
+  return {
+    service: 'online',
+  }
 })
